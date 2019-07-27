a4cc12aea7821140d809fa1f7ed635ef06198da2
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-26 20:25:08 -0700

    [ops] july26 benchmarking

--- database/dbHelpers.js
+++ database/dbHelpers.js
@@ -23,8 +23,6 @@ getChairByCollectionID = (id) => {
 
 getChairByRawId = (id) => {
   const numId = Number(id)
-  //add logic ie if between 30M and 40M, populate other ids.
-  //can get less items per collection by choosing a different string with less call points
   const queryStr = `SELECT * FROM chairs where id=${numId} or id=${numId + 10000000} or id=${numId + 20000000} or id=${numId + 30000000} or id=${numId + 40000000} `
   return connection.query(queryStr)
 }
