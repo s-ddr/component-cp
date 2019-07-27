f33b62d1a39fde1dbf7d1c5050444131676c2b49
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-27 12:04:26 -0700

    [ops] change file structure to accomidate routes, controllers, and second db

---
+++ database/mysql/dbHelpers.js
@@ -0,0 +1,32 @@
+const {Chairs} = require('./model.js');
+const connection = require('./index')
+
+//route option 1 raw query
+getChairByIdRaw = (id) => {
+  const queryStr = `SELECT * FROM chairs where collection_id=${+id}`
+  return connection.query(queryStr)
+}
+
+//native sequelize method
+getChairById = (id) => {
+  return Chairs.findAll({
+    where:
+    {
+      collection_id: Number(id)
+    },
+    raw: true
+  });
+}
+
+//route option search by native id //doesnt require indexing db
+getChairByNativeId = (id) => {
+  const numId = Number(id)
+  const queryStr = `SELECT * FROM chairs where id=${numId} or id=${numId + 10000000} or id=${numId + 20000000} or id=${numId + 30000000} or id=${numId + 40000000} `
+  return connection.query(queryStr)
+}
+
+module.exports = {
+  getChairById,
+  getChairByIdRaw,
+  getChairByNativeId
+}
