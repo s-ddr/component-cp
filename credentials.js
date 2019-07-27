1dec2dfbb182df3bb38456396f66c708a93e5f67
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-27 12:03:27 -0700

    [ops] leave somefiles untracked and some iin a legacy folder for reference

--- credentials.js
+++
@@ -1,8 +0,0 @@
-const mypw = 'hrla30-cp';
-const host = 'admin.cmd4su8z0tlg.us-west-1.rds.amazonaws.com'
-// mysql -h admin.cmd4su8z0tlg.us-west-1.rds.amazonaws.com -P 3306 -u admin -p
-
-module.exports = {
-    mypw,
-    host
-}
