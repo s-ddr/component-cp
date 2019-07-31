75367642cc0b4c514b0658574aa4149b3794a1d6
Jeremy Bishow <jeremybishow@gmail.com>
2019-07-31 16:37:03 -0700

    [feat] adds logic for max and min price

--- database/csvgen/csvgen5.js
+++ database/csvgen/csvgen5.js
@@ -7,19 +7,21 @@ const csvGen = (x) => {
   const fileStr = `database/csv/data${x}.csv`
   writer.pipe(fs.createWriteStream(fileStr))
   for (let i = 0; i < 10000000; i++) {
+    const price = faker.commerce.price();
+    const minPrice = price - Math.random() * price;
     writer.write({
-      id: (i+1)+((x-1)*10000000),
+      id: (i + 1) + ((x - 1) * 10000000),
       name: `${faker.lorem.slug()} ${faker.commerce.product()}`,
-      collection_id: i+1,
+      collection_id: i + 1,
       url: faker.random.image(),
-      max_price: faker.commerce.price(),
-      min_price: faker.commerce.price(),
+      max_price: price,
+      min_price: minPrice.toFixed(),
       limited_offer: faker.random.boolean(),
       on_sale: faker.random.boolean()
     });
   }
   writer.end()
-  console.log('done generating csv');
+  console.log('done generating csv5');
 }
 
 csvGen(5);
